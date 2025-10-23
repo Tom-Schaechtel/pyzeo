913cf6fb7329173e59e46f969a6917ff4e02752c
Tom-Schaechtel <93041131+Tom-Schaechtel@users.noreply.github.com>
2025-10-23 16:30:30 +0200

    debug find_channel

--- src/pyzeo/extension.pyx
+++ src/pyzeo/extension.pyx
@@ -237,7 +237,8 @@ def find_channels(VoronoiNetwork vornet, double channel_radius):
     for i in range(c_channels.size()):  
         channel = Channel()  
         # Copy the C++ CHANNEL data  
-        channel.thisptr[0] = c_channels[i]  
+        channel.thisptr[0] = c_channels[i]
+        print(f"DEBUG: Python channel {i} has {channel.thisptr.idMappings.size()} id mappings")
         py_channels.append(channel)  
       
     return py_channels, py_access_info
